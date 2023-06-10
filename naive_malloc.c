0dbde34fff082d3d2067e2d180b16cf0344452e8
Anna78990 <takasuanna1990@gmail.com>
2023-06-10 04:23:15 +0000

    naive_malloc

--- naive_malloc.c
+++ naive_malloc.c
@@ -5,22 +5,24 @@ typedef struct block {
     struct block* next;
 } block_t;
 
+/**
+ * naive_malloc - naive malloc
+ * @size: size of the memory to allocate
+ */
 void *naive_malloc(size_t size)
 {
-    size_t total_size = sizeof(block_t) + size;
+	size_t total_size = sizeof(block_t) + size;
+	void *user_ptr;
+	void *ptr = sbrk(total_size);
+	block_t *block;
 
-    // Allocate memory using sbrk
-    void *ptr = sbrk(total_size);
-    if (ptr == (void *)-1) {
-        return NULL;
-    }
+	if (ptr == (void *)-1)
+		return NULL;
 
-    // Create a block structure at the beginning of the allocated memory
-    block_t *block = (block_t *)ptr;
-    block->size = size;
+	block = (block_t *)ptr;
+	block->size = size;
 
-    // Calculate the user pointer
-    void *user_ptr = (void *)((char *)ptr + sizeof(block_t));
+	user_ptr = (void *)((char *)ptr + sizeof(block_t));
 
-    return user_ptr;
+	return user_ptr;
 }
